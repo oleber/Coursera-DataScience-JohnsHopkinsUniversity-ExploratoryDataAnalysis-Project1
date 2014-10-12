aef93b86906585101abe04219b24516a85629972
Marcos Rebelo <oleber@gmail.com>
2014-10-12 12:50:39 +0200

    Add plot 1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+dataRaw <- read.csv(
+  "data/household_power_consumption.txt",
+  sep=";",
+  dec=".",
+  na.strings = "?",
+  colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+)
+
+data <- dataRaw[(dataRaw$Date == "1/2/2007" | dataRaw$Date == "2/2/2007"), ]
+
+png(filename = "plot1.png", width = 480, height = 480)
+
+hist(data$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
